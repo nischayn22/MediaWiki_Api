57a15a0042ccd5a0dc5c3f1413b54f66c80830ae
Nischayn22 <nischayn22@gmail.com>
2018-06-21 09:11:19 +0530

    added composer.json

--- src/MediaWikiApi.php
+++ src/MediaWikiApi.php
@@ -1,6 +1,9 @@
 <?php
- ini_set('display_errors', 1); 
- error_reporting(E_ALL);
+
+namespace Nischayn22;
+
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
 
 global $settings;
 $settings['cookiefile'] = "cookies.tmp";
